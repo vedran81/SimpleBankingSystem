a2c2decf50ba26008a9adb632e3aea9155667c57
vex81 <vedran.smolec@gmail.com>
2022-03-21 08:34:50 +0100

    simple banking system stage 2

--- Simple Banking System/task/src/banking/Card.java
+++ Simple Banking System/task/src/banking/Card.java
@@ -3,13 +3,55 @@ package banking;
 import java.util.Random;
 
 public class Card {
-     private String cardNumber;
-     private String pin;
-     private double balance;
-     static private Random random = new Random();
+    private String cardNumber;
+    private String pin;
+    private double balance;
+    static private Random random = new Random();
+
+
+    public static boolean isValidCreditCardNumber(String cardNumber) {
+        // int array for processing the cardNumber
+        int[] cardIntArray = new int[cardNumber.length()];
+
+        for (int i = 0; i < cardNumber.length(); i++) {
+            char c = cardNumber.charAt(i);
+            cardIntArray[i] = Integer.parseInt("" + c);
+        }
+
+        for (int i = cardIntArray.length - 2; i >= 0; i = i - 2) {
+            int num = cardIntArray[i];
+            num = num * 2;  // step 1
+            if (num > 9) {
+                num = num % 10 + num / 10;  // step 2
+            }
+            cardIntArray[i] = num;
+        }
+
+        int sum = sumDigits(cardIntArray);  // step 3
+
+        System.out.println(sum);
+
+        // step 4
+        return sum % 10 == 0;
+    }
+
+    private static int sumDigits(int[] cardIntArray) {
+        int res = 0;
+        for (int i = 0; i < cardIntArray.length; i++) {
+            res = res + cardIntArray[i];
+        }
+        return res;
+    }
+
+    static public String newValidCardNumber() {
+        String tryCardNum;
+        do {
+            tryCardNum = newCardNumber();
+        } while (!isValidCreditCardNumber(tryCardNum));
+        return tryCardNum;
+    }
 
     static public String newCardNumber() {
-
         String cardNumber;
         String bin = "400000";
         int lowerAccNr = 100000;
@@ -24,7 +66,7 @@ public class Card {
     }
 
     public Card() {
-        setCardNumber(newCardNumber());
+        setCardNumber(newValidCardNumber());
         setBalance(0);
     }
 
