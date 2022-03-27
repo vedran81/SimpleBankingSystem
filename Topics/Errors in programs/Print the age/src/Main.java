8d0f86035fe8c61530f65bd622375bd84dd285bc
Vedran <vedran.smolec@gmail.com>
2022-03-27 22:28:15 +0200

    fix 3

--- Topics/Errors in programs/Print the age/src/Main.java
+++ Topics/Errors in programs/Print the age/src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public clas Main {
+    public static void main(String() args) {
 	Scanner scanner = new Scanner(System.in);
         int age = scanner.nextInt();        
     	System.out.printf("My age is %d", age);
